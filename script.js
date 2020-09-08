42d2f2cce377604e29adbe7c204d3c2be0800364
achirasarker <67404429+achirasarker@users.noreply.github.com>
2020-09-07 23:17:17 -0400

    Edited dropdown menu

---
+++ script.js
@@ -0,0 +1,7 @@
+$("closecontent").click(function() {
+  document.getElementById("navbarNav").style.height = "0%";
+});
+
+$("navbar-toggler").click(function() {
+  $("navbar-toggler").slideDown();
+})
